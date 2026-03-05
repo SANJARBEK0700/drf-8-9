412fbb86d18fd9c8183ee784670c9cb930a964a5
SANJARBEK0700 <113458249+SANJARBEK0700@users.noreply.github.com>
2026-03-05 16:18:30 +0500

    Yangi kommit

--- shared/utility.py
+++ shared/utility.py
@@ -1,128 +1,40 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.core.validators import FileExtensionValidator
-from shared.models import BaseModel
-from datetime import datetime,timedelta
-from config.settings import EMAIL_EXPIRATION_TIME, PHONE_EXPIRATION_TIME
-from rest_framework_simplejwt.tokens import RefreshToken
-import uuid
-import random
+import re
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
+from django.core.mail import send_mail
+from django.conf import settings
+
+email_regex = re.compile(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
+phone_regex = re.compile(r'^998([378]{2}|(9[013-57-9]))\d{7}$')
 
 
-
-ORDINARY_USER, ADMIN, MANAGER = ('ordinary_user', 'admin', 'manager')
-NEW, CODE_VERIFY, DONE, PHOTO_DONE = ('new', 'code_verify', 'done', 'photo_done')
-VIA_EMAIL, VIA_PHONE = ('via_email', 'via_phone')
-
-
-class CustomUser(AbstractUser,BaseModel):
-
-    USER_ROLE = (
-        (ORDINARY_USER, ORDINARY_USER),
-        (ADMIN, ADMIN),
-        (MANAGER, MANAGER)
-    )
-
-    USER_AUTH_STATUS = (
-        (NEW, NEW),
-        (CODE_VERIFY, CODE_VERIFY),
-        (DONE, DONE),
-        (PHOTO_DONE, PHOTO_DONE)
-    )
-
-    USER_AUTH_TYPE = (
-        (VIA_EMAIL, VIA_EMAIL),
-        (VIA_PHONE, VIA_PHONE)
-    )
-
-    user_role = models.CharField(max_length=20,choices=USER_ROLE,default=ORDINARY_USER)
-    auth_status = models.CharField(max_length=20,choices=USER_AUTH_STATUS,default=NEW)
-    auth_type = models.CharField(max_length=20,choices=USER_AUTH_TYPE)
-    email = models.EmailField(max_length=50, unique=True, blank=True, null=True)
-    phone_number = models.CharField(max_length=13,unique=True, blank=True, null=True)
-    photo = models.ImageField(upload_to='/user_photos/',  \
-    validators=[FileExtensionValidator(allowed_extensions=['jpg', 'png','heic'])])
-
-    def __str__(self):
-        return self.username
-
-
-    def check_username(self):
-        if not self.username:
-            temp_username=f"username{uuid.uuid4().__str__().split('-')[-1]}"
-            while CustomUser.objects.filter(username=temp_username).first().exists():
-                temp_username+=str(random.randint(0,9))
-
-            self.username=temp_username
-
-
-    def check_pass(self):
-        if not self.password:
-            temp_password=f"pass{uuid.uuid4().__str__().split('-')[-1]}"
-            self.username=temp_password
-
-    def hashing_pass(self):
-        if not self.password.startswith('pbkdf2_sha256'):
-            self.password=self.set_password(self.password)
-
-
-    def check_email(self):
-        if self.check_email:
-            email_normalize=self.email.lower()
-            self.email=email_normalize
-
-
-    def token(self):
-        refresh_token=RefreshToken.for_user(self)
-
-        data={
-            'refresh':str(refresh_token),
-            'access':str(refresh_token.access_token)
+def check_email_or_phone(user_input):
+    if re.fullmatch(phone_regex, user_input):
+        return 'phone'
+    elif re.fullmatch(email_regex, user_input):
+        return 'email'
+    else:
+        response = {
+            'status': status.HTTP_400_BAD_REQUEST,
+            'message': "email yoki telefon raqamingiz xato kiritilgan"
         }
-        return data
-
-    def generate_code(self,verify_type):
-        code=random.randint(1000,9999)
-        CODE_VERIFY.objects.create(
-            code=code,
-            user=self,
-            verify_type=verify_type
-        )
-        return code
+        raise ValidationError(response)
 
 
 
 
-    def clean(self):
-        self.check_email()
-        self.check_username()
-        self.check_pass()
-        self.hashing_pass()
-        return super().clean()
-
-    def save(self, force_insert=..., force_update=..., using=..., update_fields= ...):
-        self.clean()
-        return super().save(force_insert,force_update,using,update_fields)
-
-
-
-class CodeVerify(BaseModel):
-    VERIFY_TYPE=(
-    (VIA_EMAIL,VIA_EMAIL),
-    (VIA_PHONE,VIA_PHONE)
+def send_verification_email(email: str, code: int):
+    subject = "Email tasdiqlash kodi"
+    message = (
+        f"Assalomu alaykum!\n\n"
+        f"Ro'yxatdan o'tish uchun tasdiqlash kodingiz: {code}\n\n"
+        f"Kod {settings.EMAIL_EXPIRATION_TIME} daqiqa davomida amal qiladi.\n\n"
+        f"Agar siz ro'yxatdan o'tmagan bo'lsangiz, ushbu xabarni e'tiborsiz qoldiring."
     )
-
-    user=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
-    code=models.CharField(max_length=4)
-    verify_type=models.CharField(max_length=30, choices=VERIFY_TYPE)
-    expiration_time=models.DateTimeField()
-
-    def save(self, *args, **kwargs):
-        if self.verify_type== VIA_EMAIL:
-            self.expiration_time=datetime.now()+timedelta(minutes=EMAIL_EXPIRATION_TIME)
-        else:
-            self.expiration_time=datetime.now()+timedelta(minutes=EMAIL_EXPIRATION_TIME)
-        return super().save(*args,**kwargs)
-
-    def __str__(self):
-        return self.username
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[email],
+        fail_silently=False,
+    )
--- users/serializers.py
+++ users/serializers.py
@@ -1,43 +1,71 @@
-from rest_framework import serializers,status
-
-from .models import CODE_VERIFY,CustomUser,VIA_EMAIL,VIA_PHONE
+from rest_framework import serializers, status
 from rest_framework.exceptions import ValidationError
+from django.db.models import Q
+
+from .models import CustomUser, VIA_EMAIL, VIA_PHONE, CodeVerify
 from shared.utility import check_email_or_phone
+from shared.utility import send_verification_email
 
 
 class SignUpSerializer(serializers.ModelSerializer):
-    id=serializers.UUIDField(read_only=True,required=True)
-    auth_status=serializers.CharField(read_only=True,required=True)
-    verify_type=serializers.CharField(read_only=True,required=True)
+    id = serializers.UUIDField(read_only=True)
+    auth_status = serializers.CharField(read_only=True)
+    auth_type = serializers.CharField(read_only=True)
 
-    def __init__(self,instance=None,data=...,**kwargs):
-        super().__init__(instance,data,**kwargs)
-        super().fields['email_or_phone']=serializers.CharField(write_only=True,required=True)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email_or_phone'] = serializers.CharField(write_only=True, required=True)
 
     class Meta:
-        model=CustomUser
-        fields=['id','auth_status','verify_type']
+        model = CustomUser
+        fields = ['id', 'auth_status', 'auth_type']
 
-    @staticmethod
-    def auth_validate(user_input):
-        user_input_type= check_email_or_phone(user_input)
-        if user_input_type=='phone':
-            data={
-                'verify_type':VIA_PHONE,
-                'phone_number':user_input
-            }
 
-        elif user_input_type=='email':
-            data={
-                'verify_type':VIA_EMAIL,
-                'email':user_input
-            }
+    def validate(self, attrs):
+        email_or_phone = self.initial_data.get('email_or_phone')
+        if not email_or_phone:
+            raise ValidationError({'email_or_phone': 'Bu maydon majburiy.'})
 
+        already_exists = CustomUser.objects.filter(
+            Q(phone_number=email_or_phone) | Q(email=email_or_phone)
+        ).exists()
+        if already_exists:
+            raise ValidationError({'email_or_phone': 'Bu email yoki telefon bilan allaqachon ro\'yxatdan o\'tilgan.'})
+
+        input_type = check_email_or_phone(email_or_phone)
+        if input_type == 'phone':
+            attrs['auth_type'] = VIA_PHONE
+            attrs['phone_number'] = email_or_phone
+        elif input_type == 'email':
+            attrs['auth_type'] = VIA_EMAIL
+            attrs['email'] = email_or_phone
         else:
-            response={
-                'status':status.HTTP_400_BAD_REQUEST,
-                'message': "email yoki telefon raqamingiz xato kiritilgan"
-            }
-            raise ValidationError(response)
+            raise ValidationError({'email_or_phone': 'Email yoki telefon raqamingiz xato kiritilgan.'})
 
+
+        attrs.pop('email_or_phone', None)
+        return attrs
+
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+
+        if user.auth_type == VIA_EMAIL:
+            code = user.generate_code(VIA_EMAIL)
+            send_verification_email(user.email, code)
+
+        elif user.auth_type == VIA_PHONE:
+            code = user.generate_code(VIA_PHONE)
+        else:
+            raise ValidationError('auth_type aniqlanmadi.')
+
+        return user
+
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        tokens = instance.token()
+        data['message'] = 'Tasdiqlash kodi yuborildi.'
+        data['refresh'] = tokens['refresh']
+        data['access'] = tokens['access']
         return data
